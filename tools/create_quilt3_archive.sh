97715f8b2bd0a00c8cba313eafaf6e3c0eb81ecc
t-m <tomas.wolak@gmail.com>
2018-12-29 02:37:55 +0100

    Adding scripts to simplify building.

---
+++ tools/create_quilt3_archive.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#
+# Create tar.gz archive (quilt3) from current project
+# (usually needed for Debian build tools like debuild)
+#
+# !!! Execute in the main directory !!
+#
+
+# validate if extracted string is a valid version!
+# TO ADD
+#check_version()
+#{
+# ...
+#}
+
+
+VERSION_DEBIAN=`grep -e '^[a-z]' debian/changelog \
+            | cut -d'(' -f 2 | cut -d')' -f 1`
+VERSION=`echo $VERSION_DEBIAN | cut -d'-' -f 1`
+
+SRC_ARCHIVE="../caprice32_${VERSION}.orig.tar.gz"
+echo -e "\nCreating source archive:\n" \
+     "  version: $VERSION\n" \
+     "  archive: $SRC_ARCHIVE\n"
+
+#check_version $VERSION
+
+tar czf $SRC_ARCHIVE \
+    `ls -a | \
+     grep -v '^\.$' | \
+     grep -v '^\.\.$' | \
+     grep -v "^.git$" | \
+     grep -v debian`
